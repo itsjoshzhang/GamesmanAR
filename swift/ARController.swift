eb04fd0d59c32a1c6fd05696691e9cbf1b40a757
Josh Zhang <joshzhang@berkeley.edu>
2025-11-13 22:55:21 -0800

    smooth

--- swift/ARController.swift
+++ swift/ARController.swift
@@ -36,93 +36,100 @@ struct ARController: UIViewRepresentable {
     // MARK: - Main logic
     
     class Coordinator: NSObject, ARSessionDelegate {
-        var sceneView: ARSCNView!
-        var seenTimes: [Int: Date] = [:]
         var mutexLock = false
+        var sceneView: ARSCNView!
+        var seenNodes: [Int: (node: ArucoNode, time: Date)] = [:]
+        let busyQueue = DispatchQueue(label: "busyQueue", qos: .userInitiated)
         
-        // Runs every frame. Gets board+piece transforms,updates nodes
+        // Runs every frame. Finds board, piece, and camera transforms
         func session(_ session: ARSession, didUpdate frame: ARFrame) {
             if mutexLock { return }
             mutexLock = true
             
-            // Returns transforms for all markers based on markerSize
-            func detect(_ markerSize: Double) -> [SKWorldTransform] {
-                return ArucoCV.estimatePose(frame.capturedImage,
+            // Detects all markers, filters for board/piece transforms
+            func detectAR(isBoardMarker: Bool) -> [SKWorldTransform] {
+                let boardMarks = Constants.FixedMarkerDict.keys
+                let markerSize = isBoardMarker ? Constants.BoardMarkerSize : Constants.PieceMarkerSize
+                
+                let transforms = ArucoCV.estimatePose(frame.capturedImage,
                     withIntrinsics: frame.camera.intrinsics,
                     andMarkerSize: markerSize
                 ) as! [SKWorldTransform]
-            }
-            
-            // Filter all transforms into board and piece
-            let boardIDs = Constants.FixedMarkerDict.keys
-            let boardTransforms = detect(Constants.BoardMarkerSize).filter { boardIDs.contains(Int($0.arucoId)) }
-            let pieceTransforms = detect(Constants.PieceMarkerSize).filter {!boardIDs.contains(Int($0.arucoId)) }
-
-            if (boardTransforms + pieceTransforms).isEmpty {
-                mutexLock = false
-                return
-            }
-            
-            DispatchQueue.main.async {
-                self.updateNodes(boardTransforms, pieceTransforms, SCNMatrix4(frame.camera.transform))
-                self.mutexLock = false
-            }
-        }
-        
-        // Return node if it exists
-        func findNode(arucoId: Int) -> ArucoNode? {
-            for node in sceneView.scene.rootNode.childNodes {
-                if let node = node as? ArucoNode, node.id == arucoId {
-                    return node
+                
+                if isBoardMarker {
+                    return transforms.filter { boardMarks.contains(Int($0.arucoId)) }
+                } else {
+                    return transforms.filter {!boardMarks.contains(Int($0.arucoId)) }
                 }
             }
-            return nil
+            
+            // Some self bs, send transforms to nodes
+            busyQueue.async { [weak self] in
+                guard let self = self else { return }
+                
+                let boardTransforms = detectAR(isBoardMarker: true)
+                let pieceTransforms = detectAR(isBoardMarker: false)
+                let cameraTransform = SCNMatrix4(frame.camera.transform)
+                
+                if boardTransforms.isEmpty && pieceTransforms.isEmpty {
+                    mutexLock = false
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.updateNodes(boardTransforms, pieceTransforms, cameraTransform)
+                    self.mutexLock = false
+                }
+            }
         }
         
         // Send world (rendered) and label (relative) positions
         func updateNodes(_ boardTransforms: [SKWorldTransform], _ pieceTransforms: [SKWorldTransform], _ cameraTransform: SCNMatrix4) {
             
-            for t in (boardTransforms + pieceTransforms) {
-                let arucoId = Int(t.arucoId)
-                let worldPosition = SCNMatrix4Mult(t.transform, cameraTransform)
-                let labelPosition = findPosition(arucoId, boardTransforms, pieceTransforms)
+            for Tfm in (boardTransforms + pieceTransforms) {
+                let arucoId  = Int(Tfm.arucoId)
+                let worldPos = SCNMatrix4Mult(Tfm.transform, cameraTransform)
+                let labelPos = findPosition(arucoId, boardTransforms, pieceTransforms)
+                let currNode: ArucoNode
                 
-                // Update node if it exists, else make new
-                if let node = findNode(arucoId: arucoId) {
-                    node.setWorldTransform(worldPosition)
-                    node.createText(label: labelPosition)
+                // Update current node if it exists
+                if let tuple = seenNodes[arucoId] {
+                    currNode = tuple.node
+                    let wPos = SCNVector3(worldPos.m41, worldPos.m42, worldPos.m43)
+                    let move = SCNAction.move(to: wPos, duration: 0.1)
+                    currNode.runAction(move)
+                
+                // Otherwise make new node and text
                 } else {
-                    let node = ArucoNode(arucoId: arucoId)
-                    sceneView.scene.rootNode.addChildNode(node)
-                    node.setWorldTransform(worldPosition)
-                    node.createText(label: labelPosition)
+                    currNode = ArucoNode(arucoId: arucoId)
+                    sceneView.scene.rootNode.addChildNode(currNode)
+                    currNode.setWorldTransform(worldPos)
                 }
-                seenTimes[arucoId] = Date()
+                currNode.createText(label: labelPos)
+                seenNodes[arucoId] = (node: currNode, time: Date())
             }
             
             // Remove unseen nodes after 1sec
-            for (id, lastSeen) in seenTimes {
-                if Date().timeIntervalSince(lastSeen) > 1.0 {
-                    findNode(arucoId: id)?.removeFromParentNode()
-                    seenTimes.removeValue(forKey: id)
+            for (id, tuple) in seenNodes {
+                if Date().timeIntervalSince(tuple.time) > 1.0 {
+                    tuple.node.removeFromParentNode()
+                    seenNodes.removeValue(forKey: id)
                 }
             }
         }
         
         func findPosition(_ arucoId: Int, _ boardTransforms: [SKWorldTransform], _ pieceTransforms: [SKWorldTransform]) -> SCNVector3? {
-            
-            // If board marker -> constant pos. If no board -> nil
+            // Board markers: use known pos
             if let boardPos = Constants.FixedMarkerDict[arucoId] {
                 return SCNVector3(boardPos.x, boardPos.y, boardPos.z)
             }
-            if boardTransforms.isEmpty { return nil }
             
-            // Verify piece marker
-            guard let pieceTfm = pieceTransforms.first(where: { Int($0.arucoId) == arucoId })
+            // No board markers: return nil
+            guard !boardTransforms.isEmpty,
+                  let pieceTfm = pieceTransforms.first(where: { Int($0.arucoId) == arucoId })
             else { return nil }
             
-            // Average pos across all board markers
-            var sums = SCNVector3Zero
+            // Sum across all board markers
+            var sums = SCNVector3(0, 0, 0)
             for boardTfm in boardTransforms {
                 let boardPos = Constants.FixedMarkerDict[Int(boardTfm.arucoId)]!
                 
@@ -135,7 +142,6 @@ struct ARController: UIViewRepresentable {
                 sums.y += piecePos.y + Float(boardPos.y)
                 sums.z += piecePos.z + Float(boardPos.z)
             }
-            
             let count = Float(boardTransforms.count)
             return SCNVector3(sums.x / count, sums.y / count, sums.z / count)
         }
